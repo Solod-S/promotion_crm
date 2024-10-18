884336fa29d9fd9f9ccdbccd8b45f86269f596d4
Sergey <104456968+Solod-S@users.noreply.github.com>
2024-10-18 17:55:17 +0300

    finish dashboard markup
    finish dashboard markup

---
+++ src/app/(admin)/dashboard/@sales/default.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import React from 'react';
+
+export interface DefaultProps {}
+
+export default function Default({}: DefaultProps) {
+  return null;
+}
+
+// запасной вариант
---
+++ src/app/(admin)/dashboard/@sales/loading.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export interface LoadingPageProps {}
+
+export default async function LoadingPage({}: LoadingPageProps) {
+  return <div>Loading....</div>;
+}
